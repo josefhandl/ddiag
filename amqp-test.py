1903475ef130af8b31401422161f73fa8e36f5cc
Josef Handl <handljosef@gmail.com>
2024-01-26 11:51:00 +0100

    add ssl ignore for amqp test script

--- amqp-test.py
+++ amqp-test.py
@@ -1,14 +1,21 @@
 #!venv/bin/python3
 
+import ssl
 import socket
 from kombu import Connection
+
 host = ""
 port = 5671
 user = ""
 password = ""
 vhost = ""
+
 url = 'amqps://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, vhost)
-with Connection(url) as c:
+
+broker_use_ssl = {
+  'cert_reqs': ssl.CERT_NONE
+}
+with Connection(url, ssl=broker_use_ssl) as c:
     try:
         c.connect()
         c.close()
