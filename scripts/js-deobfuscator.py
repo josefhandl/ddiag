134f0aa4a64125fa3576ab0f95bfa7bb39e00992
Josef Handl <handljosef@gmail.com>
2024-02-27 15:50:24 +0100

    add deobfuscation script

---
+++ scripts/js-deobfuscator.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+import jsbeautifier
+
+options = jsbeautifier.default_options()
+options.indent_size = 4
+options.unescape_strings = True
+
+
+def is_potentially_obfuscated(original_code, beautified_code):
+    # Set a threshold for line count reduction (adjust as needed)
+    line_threshold = 0.8  # 80% reduction
+
+    original_lines = original_code.split('\n')
+    beautified_lines = beautified_code.split('\n')
+
+    original_line_count = len(original_lines)
+    beautified_line_count = len(beautified_lines)
+
+    line_ratio = original_line_count / beautified_line_count
+
+    return line_ratio < line_threshold
+
+def process_file(input_path, output_path):
+    with open(input_path, 'r') as file:
+        original_code = file.read()
+
+    fail = False
+
+    try:
+        beautified_code = jsbeautifier.beautify(original_code, options)
+        #beautified_code = decode_escape_sequences(beautified_code)
+    except:
+        fail = True
+
+    if is_potentially_obfuscated(original_code, beautified_code):
+        with open(output_path, 'w') as file:
+            file.write(beautified_code)
+        print(f"Deobfuscated file: {input_path}")
+    elif fail:
+        with open(output_path, 'w') as file:
+            file.write("// Beautification failed")
+    else:
+        print(f"Skipping non-obfuscated file: {input_path}")
+
+#def process_directory(input_dir, skip_dirs):
+def process_directory(input_dir):
+    for root, dirs, files in os.walk(input_dir):
+        #dirs[:] = [d for d in dirs if d not in skip_dirs]
+        for file_name in files:
+            if file_name.endswith('.js'):
+                input_path = os.path.join(root, file_name)
+                output_path = os.path.join(root, f"{file_name}.deobfuscated.js")
+                process_file(input_path, output_path)
+
+def main():
+    parser = argparse.ArgumentParser(description="Recursively beautify JavaScript files in a directory.")
+    parser.add_argument("input_directory", help="Path to the input directory containing JavaScript files.")
+    #parser.add_argument("--skip-dirs", nargs='+', default=[], help="List of directories to skip during processing.")
+    args = parser.parse_args()
+
+    input_directory = args.input_directory
+    #skip_dirs = args.skip_dirs
+
+    process_directory(input_directory)
+    #process_directory(input_directory, skip_dirs)
+    print("Beautification complete.")
+
+if __name__ == "__main__":
+    main()
